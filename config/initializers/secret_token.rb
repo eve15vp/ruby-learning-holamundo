92b6a6f4326855ee4a990b3efaebe320fbf21b85
EvE <evelyn.vega.paillacho@gmail.com>
2014-09-11 23:01:50 +0200

    Primera subida de ficheros

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolaMundo::Application.config.secret_token = '960f007ad389cfcf9cbe2e83d64e7c20698168c7b80ff4f256d55c59528b3d3cb39fb1fbf77b1b238406d9a37eb008e6e9a60205926e14ccaa88447fefa2feec'
